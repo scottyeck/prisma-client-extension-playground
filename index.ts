b023f558bd63945a8c2cb6b183a4f486ceb0fee3
Scotty Eckenthal <scott.eckenthal@gmail.com>
2024-05-23 15:12:48 -0400

    qol

--- index.ts
+++ index.ts
@@ -3,15 +3,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)}
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+}
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
